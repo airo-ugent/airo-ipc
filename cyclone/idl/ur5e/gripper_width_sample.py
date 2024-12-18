58534b7f929658a4c4fa3a35178bfb7577de0154
Mathieu De Coster <mth.decoster@gmail.com>
2024-12-18 09:19:50 +0100

    Refactor and documentation

--- cyclone/idl/ur5e/gripper_width_sample.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field, dataclass
-from cyclonedds.idl import IdlStruct
-from cyclone.cyclone_namespace import CYCLONE_NAMESPACE
-
-@dataclass
-class GripperWidthSample(IdlStruct, typename=f"{CYCLONE_NAMESPACE.UR5E_GRIPPER_WIDTH}.Msg"):
-    timestamp: float = field(metadata={"id": 0})
-
-    width: float = field(metadata={"id": 1})
