4d089a2f7acf1b1207dc9eaf167d2f120f63635e
Mathieu De Coster <mth.decoster@gmail.com>
2025-07-02 14:26:06 +0200

    Remove leftovers from port

--- airo_ipc/cyclone_shm/idl/message.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-
-from cyclonedds.idl import IdlStruct
-
-
-@dataclass
-class Message(IdlStruct, typename="Message.Msg"):
-    id: int = field(metadata={"id": 0})
-    content: str = field(metadata={"id": 1})
